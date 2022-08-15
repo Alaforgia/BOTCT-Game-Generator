a474cac988f3a85f0f0e0ccca465603029f64e01
Tony LaForgia <alforgia428@gmail.com>
2022-08-15 16:50:54 -0500

    created styled components variables/simple css added

--- pages/NumberRandomizer.tsx
+++ pages/NumberRandomizer.tsx
@@ -1,15 +1,42 @@
+import styled from "styled-components";
 
+const Wrapper = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  align-content: center;
+`;
+
+const Forms = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 10px;
+  padding: 10px;
+`;
+
+const Input = styled.input`
+  margin: 5px;
+  padding: 5px;
+`;
+
+const Button = styled.button``;
 
 function NumberRandomizer() {
   return (
     <div>
-      <main>
-        <form>
-          <input type="text"></input>
-        </form>
-      </main>
+      <Wrapper>
+        <Forms>
+          <Input type="text"></Input>
+          <Input type="text"></Input>
+          <Input type="text"></Input>
+        </Forms>
+        <Button>Create</Button>
+      </Wrapper>
     </div>
-  )
+  );
 }
 
-export default NumberRandomizer
+export default NumberRandomizer;
