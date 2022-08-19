6e0bcc8357bd21d53cdd960cb41500df269ac397
Tony LaForgia <alforgia428@gmail.com>
2022-08-19 03:35:32 -0500

    index clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,57 +2,11 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import NumberRandomizer from "./NumberRandomizer";
-import { useState } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import PlayerInputGenerator from "../src/components/PlayerInputGenerator";
 
 const Home: NextPage = () => {
-  const router = useRouter();
-  //
-  const [numOfPlayers, setNumOfPlayers] = useState(0);
-  const [players, setPlayers] = useState([]);
-  const player: any = [];
-  //
-  const onClickNumberOfPlayers = (e: any): any => {
-    const numOfPlayers = e.target.value;
-
-    setNumOfPlayers(numOfPlayers);
-    if (numOfPlayers > 0) {
-      const generateInputs: any = Array.from(Array(Number(e.target.value)).keys());
-      setPlayers(generateInputs);
-    } else {
-      setPlayers([]);
-    }
-  };
-  //
-  const addInputs = (): any => {
-    return players.map((persons: any): any => (
-      <input
-        name="inputs"
-        key={persons}
-        type="text"
-        // value={player}
-        // onChange={onClickNumberOfPlayers}
-      ></input>
-    ));
-  };
-  //
-  const handleSubmit = (event: any): void => {
-    event.preventDefault();
-    // onClickNumberOfPlayers(event);
-    // addInputs();
-    console.log("you clicked Submit", event);
-  };
-  //
-
-  const handleClick = (e: any) => {
-    e.preventDefault();
-    router.push("/NumberRandomizer");
-    onClickNumberOfPlayers(e);
-  };
-
   return (
+    <>
     <div className={styles.container}>
       <Head>
         <title>BOTCT Companion</title>
@@ -64,20 +18,10 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://bloodontheclocktower.com/">Blood On The Clock Tower</a> Companion!
         </h1>
-
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={numOfPlayers} onChange={onClickNumberOfPlayers}></input>
-        </form>
-        {/* <Link href="/NumberRandomizer"> */}
-        <button type="submit" onClick={handleClick}>
-          SUBMIT
-        </button>
-        {/* </Link> */}
-        {/* <h3>{addInputs()}</h3> */}
-
-        <form>{players.length ? <div>{addInputs()}</div> : null}</form>
       </main>
     </div>
+      <PlayerInputGenerator />
+      </>
   );
 };
 
