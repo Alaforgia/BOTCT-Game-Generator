da4492512aa57637db28182e3601d543dd111d98
Tony LaForgia <alforgia428@gmail.com>
2022-08-20 00:54:16 -0500

    useContext is returning undefined where I want it to render

--- src/components/PlayerInputGenerator.tsx
+++ src/components/PlayerInputGenerator.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useContext, createContext } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
+import NumberRandomizer from "../../pages/NumberRandomizer";
 
 //@ts-ignore
 export const PlayerContext: any = createContext();
@@ -10,7 +12,6 @@ function PlayerInputGenerator(): JSX.Element {
   const router = useRouter();
   const [numOfPlayers, setNumOfPlayers] = useState(0);
   const [players, setPlayers] = useState([]);
-  const value = players;
   const player: any = [];
   //
   const onClickNumberOfPlayers = (e: any): any => {
@@ -38,39 +39,58 @@ function PlayerInputGenerator(): JSX.Element {
 
   const handleSubmit = (event: any): void => {
     event.preventDefault();
-    // onClickNumberOfPlayers(event);
+    // @ts-ignore
+    onClickNumberOfPlayers();
     // addInputs();
     console.log("you clicked Submit", event);
   };
   //
 
-  const handleClick = (e: any) => {
-    e.preventDefault();
+  const handleClick = (event: any) => {
+    event.preventDefault();
+    // @ts-ignore
+    // onClickNumberOfPlayers()
     router.push("/NumberRandomizer");
-    onClickNumberOfPlayers(e);
+    console.log("you clicked handleClick", event);
+    console.log("numOfPLayers = ", numOfPlayers);
   };
-  //@ts-ignore
+  // @ts-ignore
+  // console.log(" onClickNumberOfPlayers = ", onClickNumberOfPlayers());
+  // @ts-ignore
+  // console.log("numOfPLayers = ", numOfPlayers);
+  // @ts-ignore
+  const inputs = players.length ? <div>{addInputs()}</div> : null;
+  console.log("inputs is =", inputs);
+
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={numOfPlayers} onChange={onClickNumberOfPlayers}></input>
+        <input
+          type="text"
+          // defaultValue={numOfPlayers}
+          value={numOfPlayers}
+          onChange={onClickNumberOfPlayers}
+        ></input>
+        <button type="submit" onClick={handleClick}>
+          SUBMIT
+        </button>
       </form>
       {/* <Link href="/NumberRandomizer"> */}
-      <button type="submit" onClick={handleClick}>
-        SUBMIT
-      </button>
       {/* </Link> */}
-      {/* <h3>{addInputs()}</h3> */}
-      {/* <form> */}
+      <form>
         {/* 
       //@ts-ignore */}
-        <PlayerContext value={value}>
-          {/* {players.length ? <div>{addInputs()}</div> : null} */}
-          {addInputs()}
+        <PlayerContext.Provider value={inputs}>
+          {/* {players.length ? <div>{inputs}</div> : null} */}
           {/* 
       //@ts-ignore */}
-        </PlayerContext>
-      {/* </form> */}
+          {/* <h3>{addInputs(inputs)}</h3> */}
+          {/* <NumberRandomizer  /> */}
+          {/* <div>{inputs}</div> */}
+          {/* 
+      //@ts-ignore */}
+        </PlayerContext.Provider>
+      </form>
     </>
   );
 }
