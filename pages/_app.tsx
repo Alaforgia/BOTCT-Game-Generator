74e8766e536663f4f3dbb4c3ca74baed7646eca7
spaceFitzpa <louie.fitzpatrick@space150.com>
2022-08-23 17:37:45 -0500

    Wrapping app in storeContextProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 // import {} from "styled-components/cssprop";
 import type { AppProps } from "next/app";
+import { StoreContextProvider } from "../src/context/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return  (
+    <StoreContextProvider>
+      <Component {...pageProps} />
+    </StoreContextProvider>
+  )
 }
 
 export default MyApp;
