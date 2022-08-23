81a0fea41f400e31c58526dab3cb7926514fdb04
spaceFitzpa <louie.fitzpatrick@space150.com>
2022-08-23 17:28:40 -0500

    updated useNumberRandomizer hook naming & comments

--- src/hooks/useNumberRandomizer.ts
+++ src/hooks/useNumberRandomizer.ts
@@ -1,4 +1,9 @@
-const useNumberRandomizer = (currentArrayOfNumbers: number[]): number => {
+/**
+ * 
+ * @param currentPlayerIdsInGame - array of numbers, currently in the game
+ * @returns 
+ */
+const useNumberRandomizer = (currentPlayerIdsInGame: number[]): number => {
   // function to generate a random number
   const randomNumberGenerator = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -8,7 +13,7 @@ const useNumberRandomizer = (currentArrayOfNumbers: number[]): number => {
   let randomNumber = randomNumberGenerator(1, 10);
 
   // Check if the random number is already in the array
-  while (currentArrayOfNumbers.includes(randomNumber)) {
+  while (currentPlayerIdsInGame.includes(randomNumber)) {
     randomNumber = randomNumberGenerator(1, 10);
   }
 
