bb28fd7ba0682d8b0bac57e6698ed3f5eb88a9ef
Tony LaForgia <alforgia428@gmail.com>
2022-08-31 22:35:54 -0500

    added createContext to PLayerContext

--- pages/PlayerContext.tsx
+++ pages/PlayerContext.tsx
@@ -1,40 +1,44 @@
 import { useState, useContext, createContext, useRef, useEffect } from "react";
+import { useRouter } from "next/router";
+
+export const InputContext: any = createContext([]);
+
+export const InputContextProvider = ({ children }: any): any => (
+  <InputContext.Provider value={useState([])}>{children}</InputContext.Provider>
+);
 
 function PlayerContext() {
-  const prevNumOfPlayers: any = useRef([]);
+  const router = useRouter();
+  const prevNumOfPlayers: any = useRef(InputContext);
   const [numOfPlayers, setNumOfPlayers] = useState(0);
   const [players, setPlayers] = useState(prevNumOfPlayers);
 
-  // const onClickNumberOfPlayers = (e: any): any => {
-  //   const numOfPlayers = prevNumOfPlayers;
-
-  //   setNumOfPlayers(numOfPlayers);
-  //   if (numOfPlayers > 0) {
-  //     const generateInputs: any = Array.from(Array(Number(numOfPlayers)).keys());
-  //     setPlayers(generateInputs);
-  //   } else {
-  //     setPlayers([]);
-  //   }
-  // };
+  const PlayerCountInput = (props: any) => {
+    return (
+      <>
+        <input ref={prevNumOfPlayers} value={props.value} type="text" name="players"></input>
+      </>
+    );
+  };
 
   const addInputs = (): any => {
     // @ts-ignore
-    // const [numOfPlayersInputs, setNumOfPlayersInputs] = useContext(PlayerContext);
-    return players.map((persons: any, index: any): any => (
-      <input
-        name="newInputs"
-        key={index.id}
-        type="text"
-        // defaultValue={persons}
-        // onChange={onClickNumberOfPlayers}
-      ></input>
+    // const players = prevNumOfPlayers.current.value;
+    const playerInputs = players.map((persons: any, index: any): any => (
+      <PlayerCountInput name="newInputs" key={persons.toString()} id={persons.id} type="text" />
+      // <div>{persons}</div>
+      // <div>{index}</div>
+      // <div>{index.key}</div>
     ));
+
+    return <div>{playerInputs}</div>;
   };
 
   const handleClick = () => {
-    prevNumOfPlayers.current.value;
+    // prevNumOfPlayers.current.value;
     console.log(prevNumOfPlayers.current.value);
     const numOfPlayers = prevNumOfPlayers.current.value;
+    console.log("START of setNumOfPlayers");
 
     setNumOfPlayers(numOfPlayers);
     if (numOfPlayers > 0) {
@@ -43,35 +47,17 @@ function PlayerContext() {
     } else {
       setPlayers([]);
     }
-    //@ts-ignore
-    // console.log("What is onClickNumberOfPlayers? = ", onClickNumberOfPlayers());
-    //@ts-ignore
-    // onClickNumberOfPlayers();
-    //@ts-ignore
-    // addInputs();
-
-    return (
-      <form>
-        <input
-          type="text"
-          // defaultValue={numOfPlayers}
-          // ref={prevNumOfPlayers}
-          // value={numOfPlayers}
-          // onChange={(e) =>
-          //   //@ts-ignore
-          //   setNumOfPlayers(e.target.value)
-          // }
-          // onChange={onClickNumberOfPlayers}
-        ></input>
-      </form>
-    );
+    console.log("END of setNumOfPlayers");
+    router.push("/NumberRandomizer");
   };
   return (
     <div>
-      <input ref={prevNumOfPlayers} type="text" name="players" />
+      <PlayerCountInput />
+      {/* <input ref={prevNumOfPlayers} type="text" name="players" /> */}
       <button onClick={handleClick}>SUBMIT</button>
-      {players.length ? <div>{addInputs()}</div> : null}
-      {/* {numOfPlayers} */}
+      {/* <InputContextProvider value={prevNumOfPlayers}> */}
+        {players.length ? <div>{addInputs()}</div> : null}
+        {/* </InputContextProvider> */}
     </div>
   );
 }
