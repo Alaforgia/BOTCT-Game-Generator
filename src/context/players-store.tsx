350aea4aea8a2abc4772a6af2af86e52748c95f0
Tony LaForgia <alforgia428@gmail.com>
2022-09-01 17:42:51 -0500

    created new store component for initial player count input

---
+++ src/context/players-store.tsx
@@ -0,0 +1,38 @@
+import { useState, useRef, createContext, useContext } from "react";
+
+const usePlayersStore = () => {
+  const prevNumOfPlayers: any = useRef();
+  const [numOfPlayers, setNumOfPlayers] = useState(0);
+  const [players, setPlayers] = useState(prevNumOfPlayers);
+
+  return {
+    players,
+    numOfPlayers,
+    // Initial input that generate inputs based on number value given
+    addNumOfPlayers: () => {
+      const numOfPlayers = prevNumOfPlayers.current.value;
+      setNumOfPlayers(numOfPlayers);
+      if (numOfPlayers > 0) {
+        const generateInputs: any = Array.from(Array(Number(numOfPlayers)).keys());
+        setPlayers(generateInputs);
+      } else {
+        setPlayers([]);
+      }
+    },
+    //
+  };
+};
+
+export const PlayersStoreContext = createContext<ReturnType<typeof usePlayersStore> | null>(null);
+
+export const StoreContextProvider = ({ children }: { children: React.ReactNode }) => {
+  return <PlayersStoreContext.Provider value={usePlayersStore()}>{children}</PlayersStoreContext.Provider>;
+};
+
+export const useAddPlayers = () => {
+  return useContext(PlayersStoreContext)?.addNumOfPlayers;
+};
+
+export const useGetPlayers = () => {
+  return useContext(PlayersStoreContext)?.players;
+};
