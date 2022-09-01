51e0b2a2ac6b30c4bc11e7d083fe76ea6b22213c
Tony LaForgia <alforgia428@gmail.com>
2022-08-31 22:36:31 -0500

    wrapped _app with provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import "../styles/globals.css";
 // import {} from "styled-components/cssprop";
 import type { AppProps } from "next/app";
 import { StoreContextProvider } from "../src/context/store";
+import { InputContextProvider } from "./PlayerContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return  (
+  return (
     // <StoreContextProvider>
+    <InputContextProvider>
       <Component {...pageProps} />
+    </InputContextProvider>
     //{/* </StoreContextProvider> */}
-  )
+  );
 }
 
 export default MyApp;
