5747ceecbc8e8f7d2dc597dbbb543b0a305d27ca
Tony LaForgia <alforgia428@gmail.com>
2022-09-01 19:57:09 -0500

    players store changes

--- src/context/players-store.tsx
+++ src/context/players-store.tsx
@@ -1,15 +1,16 @@
 import { useState, useRef, createContext, useContext } from "react";
 
 const usePlayersStore = () => {
-  const prevNumOfPlayers: any = useRef();
+  const prevNumOfPlayers: any = useRef([]);
   const [numOfPlayers, setNumOfPlayers] = useState(0);
   const [players, setPlayers] = useState(prevNumOfPlayers);
 
   return {
     players,
     numOfPlayers,
+    prevNumOfPlayers,
     // Initial input that generate inputs based on number value given
-    addNumOfPlayers: () => {
+    addNumOfPlayers: (): any => {
       const numOfPlayers = prevNumOfPlayers.current.value;
       setNumOfPlayers(numOfPlayers);
       if (numOfPlayers > 0) {
@@ -18,24 +19,37 @@ const usePlayersStore = () => {
       } else {
         setPlayers([]);
       }
+      console.log("Clicked");
     },
     //
+    addInputs: (): any => {
+      // const playerInputs =
+      // players.map((player: any, index: any): any => (
+      //   <input ref={prevNumOfPlayers} name="newInputs" key={player.toString()} id={player.id} type="text" />
+      // ));
+      players.map((player: any) => player.id);
+
+      // setPlayers([...players]);
+    },
   };
 };
 
 const PlayersStoreContext = createContext<ReturnType<typeof usePlayersStore> | null>(null);
 
-export const StoreContextProvider = ({ children }: { children: React.ReactNode }) => {
+export const PlayersStoreContextProvider = ({ children }: any) => {
   return <PlayersStoreContext.Provider value={usePlayersStore()}>{children}</PlayersStoreContext.Provider>;
 };
 
-export const useAddPlayers = () => {
-  return useContext(PlayersStoreContext)?.addNumOfPlayers;
+export const useAddNumOfPlayers = (): any => {
+  useContext(PlayersStoreContext)?.addNumOfPlayers;
 };
 
 export const useGetPlayers = () => {
-  return useContext(PlayersStoreContext)?.players;
+  useContext(PlayersStoreContext)?.players;
 };
 
 export const useNumOfPlayers = () => useContext(PlayersStoreContext)?.numOfPlayers;
 
+export const useAddInputs = () => useContext(PlayersStoreContext)?.addInputs;
+
+// export const usePrevNumOfPlayers = () => useContext(PlayersStoreContext)?.prevNumOfPlayers;
