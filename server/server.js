4667634fcc3677beab683820ded91e035f94a9c1
Tony LaForgia <alforgia428@gmail.com>
2022-09-20 18:36:04 -0500

    added mongo client to server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const next = require("next");
+const mongo = require("mongodb").MongoClient
 
 const port = 3000;
 const dev = process.env.NODE_ENV !== "production";
