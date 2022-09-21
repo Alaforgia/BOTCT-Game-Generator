fe09245578810db909e97737f4a8239f731d62d3
Tony LaForgia <alforgia428@gmail.com>
2022-09-20 20:06:22 -0500

    attempting mongo connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const next = require("next");
-const mongo = require("mongodb").MongoClient
+const mongo = require("mongodb").MongoClient;
 
-const port = 3000;
+const uri = process.env.MONGODB_URI;
+const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+
+// import clientPromise from "./db/conn.js";
+
+// app.use(cors());
+
+// app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
 
 app.prepare().then(() => {
   const server = express();
@@ -15,6 +26,14 @@ app.prepare().then(() => {
   });
 
   server.listen(port, (err) => {
+    // clientPromise(function (err) {
+    //   if (err) throw err;
+    //   console.log(`> MONGO READY ON ${port}`);
+    // });
+    dbo.connectToServer(function (err) {
+      if (err) throw err;
+      console.log(`> MONGO READY ON ${port}`);
+    });
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
