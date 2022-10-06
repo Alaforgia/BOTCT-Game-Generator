de166be0ce74f9bfea54cb0a11b50c59a5710e3f
Tony LaForgia <alforgia428@gmail.com>
2022-10-06 00:24:06 -0500

    changes to record

--- server/routes/record.js
+++ server/routes/record.js
@@ -20,3 +20,65 @@ recordRoutes.route("/list").get(async function (req, res) {
       }
     });
 });
+
+
+// This section will help you create a new document.
+recordRoutes.route("/listings/recordSwipe").post(function (req, res) {
+  const dbConnect = dbo.getDb();
+  const matchDocument = {
+    listing_id: req.body.id,
+    last_modified: new Date(),
+    session_id: req.body.session_id,
+    direction: req.body.direction
+  };
+
+  dbConnect
+    .collection("matches")
+    .insertOne(matchDocument, function (err, result) {
+      if (err) {
+        res.status(400).send("Error inserting matches!");
+      } else {
+        console.log(`Added a new match with id ${result.insertedId}`);
+        res.status(204).send();
+      }
+    });
+});
+
+
+// This section will help you update a document by id.
+recordRoutes.route("/listings/updateLike").post(function (req, res) {
+  const dbConnect = dbo.getDb();
+  const listingQuery = { _id: req.body.id };
+  const updates = {
+    $inc: {
+      likes: 1
+    }
+  };
+
+  dbConnect
+    .collection("listingsAndReviews")
+    .updateOne(listingQuery, updates, function (err, _result) {
+      if (err) {
+        res.status(400).send(`Error updating likes on listing with id ${listingQuery.id}!`);
+      } else {
+        console.log("1 document updated");
+      }
+    });
+});
+
+
+// This section will help you delete a record.
+recordRoutes.route("/listings/delete/:id").delete((req, res) => {
+  const dbConnect = dbo.getDb();
+  const listingQuery = { listing_id: req.body.id };
+
+  dbConnect
+    .collection("listingsAndReviews")
+    .deleteOne(listingQuery, function (err, _result) {
+      if (err) {
+        res.status(400).send(`Error deleting listing with id ${listingQuery.listing_id}!`);
+      } else {
+        console.log("1 document deleted");
+      }
+    });
+});
