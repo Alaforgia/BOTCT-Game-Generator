c2d175504bbed5207ac56aac16a89054a45d5d72
Tony LaForgia <alforgia428@gmail.com>
2022-10-06 18:27:19 -0500

    type interface created

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,8 @@
-export interface I {}
+export interface IGameData {
+    _id: number,
+    name: string,
+    classes: string[] | number,
+    class_id: number,
+    class_name: string ,
+    class_ability: string,
+};
