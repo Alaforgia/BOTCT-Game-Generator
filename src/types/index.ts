cece26193f85be71fe2758fca88be923e9ced025
Tony LaForgia <alforgia428@gmail.com>
2022-10-06 19:17:43 -0500

    typescript interface update

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,9 @@
 export interface IGameData {
-    _id: number,
-    name: string,
-    classes: string[] | number,
-    class_id: number,
-    class_name: string ,
-    class_ability: string,
-};
+  game_types: string | number;
+  _id: number;
+  name: string;
+  classes: string[] | number;
+  class_id: number;
+  class_name: string;
+  class_ability: string;
+}
