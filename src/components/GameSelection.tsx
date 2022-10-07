74de7159d9fd3b32d353134a921f9f7b07f63625
Tony LaForgia <alforgia428@gmail.com>
2022-10-06 19:17:55 -0500

    SSR test

--- src/components/GameSelection.tsx
+++ src/components/GameSelection.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from "react";
+import clientPromise from "../../server/db/conn";
+// import { IGameData } from "../types";
 
-
-
-
-export default function GameSelection() {
+export default function GameSelection({ game_types }: any): any {
   return (
-    <div>GameSelection</div>
-  )
+    <>
+      <div>
+        <h2>Select Game Type</h2>
+        <div>{game_types.map((type: any)=>{
+          <li>
+            <h3>{type.name}</h3>
+          </li>
+        })}</div>
+      </div>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  try {
+    const client = await clientPromise;
+    const db = client.db("game_data");
+
+    const game_types = await db.collection("game_types").find({}).toArray();
+    return {
+      props: { game_types: JSON.parse(JSON.stringify(game_types)) },
+    };
+  } catch (e) {
+    console.error(e);
+  }
 }
