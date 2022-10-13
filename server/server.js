4e8d911172fe7167b89ffa9f1e37d20a3b5ac114
Tony LaForgia <alforgia428@gmail.com>
2022-10-12 21:50:02 -0500

    changes to server.js

--- server/server.js
+++ server/server.js
@@ -6,17 +6,41 @@ const { MongoClient } = require("mongodb");
 
 const connectionString = process.env.MONGODB_URI;
 
-nextApp.prepare().then(() => {
-  // express code here
-  const app = express()
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use('/api', require('./routes/record')) 
-  app.get('*', (req,res) => {
-      return handle(req,res) // for all the react stuff
-  })
-  app.listen(PORT, err => {
-      if (err) throw err;
-      console.log(`ready at http://localhost:${PORT}`)
-  })
-})
+require("dotenv").config({ path: "../server/.env" });
+
+const PORT = process.env.PORT || 3000;
+
+const dev = process.env.NODE_ENV !== "production";
+// const app = next({ dev });
+const app = express();
+// const handle = app.getRequestHandler();
+
+const dbo = require("./db/conn");
+app.use(express.json());
+let mainRoutes = require("./routes/record");
+app.use(mainRoutes);
+
+app.get("/:id", (req, res) => {
+  console.log(req);
+  res.send("what is this params =", req.params);
+  const queryParams = req.params.id;
+  app.render(queryParams);
+  // return handle(req, res);
+});
+
+dbo.connectToServer(function (err) {
+  if (err) throw err;
+});
+
+app.listen(PORT, (err) => {
+  console.log(`ready at http://localhost:${PORT}`);
+});
+// app.prepare().then(() => {
+//   // express code here
+//   // app.use(bodyParser.json());
+//   // app.use(bodyParser.urlencoded({ extended: true }));
+//   // app.use(mainRoutes);
+//   // const app = express;
+
+// });
+module.exports = (app) => {};
--- src/components/GameSelection.tsx
+++ src/components/GameSelection.tsx
@@ -3,33 +3,33 @@ import clientPromise from "../../server/db/conn";
 // import { IGameData } from "../types";
 
 export default function GameSelection({ game_types }: any): any {
-  return (
-    <>
-      <div>
-        <h2>Select Game Type</h2>
-        <div>
-          {game_types.map((type: any) => {
-            <li>
-              <h3>{type.name}</h3>
-            </li>;
-          })}
-        </div>
-      </div>
-    </>
-  );
+  // return (
+  //   <>
+  //     <div>
+  //       <h2>Select Game Type</h2>
+  //       <div>
+  //         {game_types.map((type: any) => {
+  //           <li>
+  //             <h3>{type.name}</h3>
+  //           </li>;
+  //         })}
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 }
 
-export async function getServerSideProps() {
-  try {
-    const client = await clientPromise;
-    const db = client.db("game_data");
+// export async function getServerSideProps() {
+//   try {
+//     const client = await clientPromise;
+//     const db = client.db("game_data");
 
-    const game_types = await db.collection("game_types").find({}).toArray();
-    return {
-      props: { game_types: JSON.parse(JSON.stringify(game_types)) },
-    };
-  } catch (e) {
-    console.error(e);
-    console.log("MongoDB data fetch not working");
-  }
-}
+//     const game_types = await db.collection("game_types").find({}).toArray();
+//     return {
+//       props: { game_types: JSON.parse(JSON.stringify(game_types)) },
+//     };
+//   } catch (e) {
+//     console.error(e);
+//     console.log("MongoDB data fetch not working");
+//   }
+// }
