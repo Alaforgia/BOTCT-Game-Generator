2942e8c4902d313b19db3d567df63afca99ac22f
Tony LaForgia <alforgia428@gmail.com>
2022-11-02 21:19:46 -0500

    changes to record

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const router = express.Router();
+// const controllers = require('../controllers/index')
 
-// recordRoutes is an instance of the express router.
-// We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path /listings.
-// const dbo = require("../db/conn");
+// const routes = [""];
 
-// This section will help you get a list of all the documents.
-const recordRoutes = express.Router();
-recordRoutes.route("/list").get(async function (req, res) {
-  let dbConnect = dbo.getDb();
+router.get("/", (req, res) => {
+  const dbConnect = dbo.getDB();
+  res.status(200).send("This is getting the game_data");
 
   dbConnect
     .collection("game_types")
@@ -17,70 +14,20 @@ recordRoutes.route("/list").get(async function (req, res) {
     .limit(50)
     .toArray(function (err, result) {
       if (err) {
-        res.status(400).send("Error fetching listings!");
+        res.status(400).send("Error fetching game_type data, but it is getting here!");
       } else {
         res.json(result);
       }
     });
 });
-
-// This section will help you create a new document.
-// recordRoutes.route("/").post(function (req, res) {
-//   const dbConnect = dbo.getDb();
-//   const matchDocument = {
-//     game_id: req.body.id,
-//     last_modified: new Date(),
-//     session_id: req.body.session_id,
-//     direction: req.body.direction
-//   };
-
-//   dbConnect
-//     .collection("matches")
-//     .insertOne(matchDocument, function (err, result) {
-//       if (err) {
-//         res.status(400).send("Error inserting matches!");
-//       } else {
-//         console.log(`Added a new match with id ${result.insertedId}`);
-//         res.status(204).send();
-//       }
-//     });
+// routes.forEach((routeName) => {
+// const childRouter = express.Router();
+// router.use(`../${routeName}`, childRouter);
+// require(`../${routeName}`)({ router: childRouter, controllers })
 // });
-
-// // This section will help you update a document by id.
-// recordRoutes.route("/").post(function (req, res) {
-//   const dbConnect = dbo.getDb();
-//   const gameQuery = { _id: req.body.id };
-//   const updates = {
-//     $inc: {
-//       likes: 1
-//     }
-//   };
-
-//   dbConnect
-//     .collection("listingsAndReviews")
-//     .updateOne(listingQuery, updates, function (err, _result) {
-//       if (err) {
-//         res.status(400).send(`Error updating likes on listing with id ${listingQuery.id}!`);
-//       } else {
-//         console.log("1 document updated");
-//       }
-//     });
-// });
-
-// // This section will help you delete a record.
-// recordRoutes.route("/listings/delete/:id").delete((req, res) => {
-//   const dbConnect = dbo.getDb();
-//   const listingQuery = { listing_id: req.body.id };
-
-//   dbConnect
-//     .collection("listingsAndReviews")
-//     .deleteOne(listingQuery, function (err, _result) {
-//       if (err) {
-//         res.status(400).send(`Error deleting listing with id ${listingQuery.listing_id}!`);
-//       } else {
-//         console.log("1 document deleted");
-//       }
-//     });
-// });
-
-module.exports = recordRoutes;
+router.get("*", function (req, res) {
+  res.send("Is this showing up? (record.js)");
+  console.log("What is res/req = ", res, req);
+  res.status(400).json({ error: "Not a valid endpoint" });
+});
+module.exports = router;
