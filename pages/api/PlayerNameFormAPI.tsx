1587ca88338b9bfb6662a7c7ea3fb9857f46fdeb
Tony LaForgia <alforgia428@gmail.com>
2022-11-20 15:41:05 -0600

    player name api json test

--- pages/api/PlayerNameFormAPI.tsx
+++ pages/api/PlayerNameFormAPI.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function PlayerNameFormAPI(req: any, res: any) {
+  const body = req.body;
+  console.log("body => ", body);
 
-const body = req.body
-
+  res.status(200).json({ data: `${body.first}` });
 
   return <div>PlayerNameFormAPI</div>;
 }
