3ba97d676d44674656f037efe2630a99f012c63e
Tony LaForgia <alforgia428@gmail.com>
2023-01-30 17:49:11 -0600

    react hook forms

--- pages/PlayerNumberSelect.tsx
+++ pages/PlayerNumberSelect.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-let maxNum;
-function PlayerNumberSelect() {
-  return (
-    
-  )
+interface IPlayerCount {
+  example: string;
+  maxNum: number;
 }
 
-export default PlayerNumberSelect
+let maxNum: number;
+function PlayerNumberSelect() {
+  if (maxNum > 0 && Number.isFinite(Number(maxNum))) {
+    maxNum = Math.round(Number(maxNum));
+  }
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IPlayerCount>();
+  const onSubmit: SubmitHandler<IPlayerCount> = (data) => console.log(data);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input defaultValue="test" {...register("example")} />
+        <input type="submit" />
+      </form>
+    </>
+  );
+}
+
+export default PlayerNumberSelect;
