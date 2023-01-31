8fb5f6b2febf58f1e217c60c0489a865c37344e5
Tony LaForgia <alforgia428@gmail.com>
2023-01-30 18:28:22 -0600

    number successful on submit

--- pages/PlayerNumberSelect.tsx
+++ pages/PlayerNumberSelect.tsx
@@ -2,27 +2,39 @@ import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 interface IPlayerCount {
-  example: string;
   maxNum: number;
+  numOfPlayers: number[];
 }
 
 let maxNum: number;
+let numOfPlayers: number[] = [];
+//
 function PlayerNumberSelect() {
   if (maxNum > 0 && Number.isFinite(Number(maxNum))) {
     maxNum = Math.round(Number(maxNum));
   }
+
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm<IPlayerCount>();
-  const onSubmit: SubmitHandler<IPlayerCount> = (data) => console.log(data);
-
+  const onSubmit: SubmitHandler<IPlayerCount> = (data) => {
+    maxNum = data.maxNum;
+    if (maxNum && !isNaN(maxNum) && maxNum > 0) {
+      if (!numOfPlayers.includes(maxNum)) {
+        numOfPlayers.push(maxNum);
+      }
+    } else if (maxNum <= 0 || isNaN(maxNum)) {
+      console.log("That is not a valid number!");
+    }
+    console.log(data);
+  };
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input defaultValue="test" {...register("example")} />
+        <input defaultValue="Enter Player Number" {...register("maxNum")} />
         <input type="submit" />
       </form>
     </>
